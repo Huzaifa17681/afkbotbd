57772cf39d6b89bdb22ce61a79437822f84a2df5
Huzaifa17681 <113714891+Huzaifa17681@users.noreply.github.com>
2024-09-24 22:01:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-
 const mineflayer = require("mineflayer");
+//const inventoryViewer = require('mineflayer-web-inventory')
 const options = {
   host: "mc.blockdrop.org",
-  username: "zinx",
+  username: "villagers",
   version: "1.20",
 };
 
-const bot = mineflayer.createBot(options);
-bot.once("login", () => {
+
+var bot = mineflayer.createBot(options);
+
+bindEvents(bot);
+//inventoryViewer(bot)
+function bindEvents(bot) {
+    bot.once("login", () => {
   console.log("Joined");
   LoginF();
 });
-
-bot.on("message", (message) => {
+    bot.on("message", (message) => {
   console.log("[Chat/Server] " + message.toString());
 });
 
+  bot.on("resourcePack", (url, hash) => {
+    console.log(url, hash)
+    bot.acceptResourcePack()
+})
 function LoginF() {
-  bot.chat("/login zinx");
+  bot.chat("/login huzaifa");
 }
 
 async function sleep(ms) {
@@ -25,22 +33,46 @@ async function sleep(ms) {
 }
 
 bot.once("spawn", async function () {
-  await sleep(4000);
-  bot.setQuickBarSlot(4);
-  await sleep(4000);
-  bot.activateItem();
+  await sleep(1200);
+ // bot.setQuickBarSlot(4);
+  //await sleep(1200);
+  //bot.activateItem();
+  bot.chat("/server survival");
+    //bot.on("windowOpen", async function (window) {
+      // Added async here
+      //await sleep(1200);
+      //console.log(window);
+      await sleep(2000);
+   //   bot.clickWindow(14, 0, 0);
+    //  bot.clickWindow(14, 0, 0);
+   //   await sleep(5000);
+   
+      bot.chat("/tpa GamerBoy321123");
+      bot.chat("/tpahere GamerGenix");
 
-  bot.on("windowOpen", async function (window) {
-    // Added async here
-    await sleep(4000);
-    bot.clickWindow(14, 0, 0);
-    bot.clickWindow(14, 0, 0);
-    await sleep(4000);
-    bot.chat("/msg GamerBoy321123 im on !!");
-  });
+      await sleep(10000);
+      bot.chat("/afk");
+      
+    
+   // });
 });
-
 // Fixed event name from 'messagestr' to 'message'
 bot.on("message", function (message) {
   console.log("Received message:", message.toString());
+});
+}
+bot.on('end', function(reason) { 
+	console.log("I got end for", reason, "lol");
+	bot = mineflayer.createBot(options);
+    bindEvents(bot);
+});
+bot.on('kicked', function(reason) {
+    console.log("I got kicked for", reason, "lol");
+    bot = mineflayer.createBot(options);
+    bindEvents(bot);
+});
+bot.on('error', function(reason) {
+    console.log("I got error for", reason, "lol");
+    bot = mineflayer.createBot(options);
+    bindEvents(bot);
 });
